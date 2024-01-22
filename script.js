80dfd0bf611cb47031c3a540bfb187cf11794994
itsshawondevs <shawonuser24@gmail.com>
2024-01-22 22:54:15 +0600

    upgrade

--- script.js
+++ script.js
@@ -4,18 +4,28 @@
 //     console.log(i +". "+ `${students[i]}`);
 // }
 
-let number = 10
+// let number = 10
 
-for(let i = 1; i<= 10; i++){
-    console.log(number + " x " + i + " = " + number*i);
-}
+// for(let i = 1; i<= 10; i++){
+//     console.log(number + " x " + i + " = " + number*i);
+// }
 
 let input = document.querySelector(".in")
 let btn = document.querySelector(".btn")
 let result = document.querySelector(".result")
+let numberregex = "NaN|-?((\d*\.\d+|\d+)([Ee][+-]?\d+)?|Infinity)"
 
 btn.addEventListener("click",function(){
-    for(let i = 1; i <= 10; i++){
-        result.innerHTML = (input.value + " x " + i + " = " + input.value*i);
+    if(input.value == ""){
+        input.style.border = "1px solid red"
+        result.innerHTML = ""
+    }else if(input.value.match(numberregex)){
+        input.style.border = "1px solid red"
+    }
+    else{
+        input.style.border = ""
+        for(let i = 1; i <= 10; i++){
+            result.innerHTML = (input.value + " x " + i + " = " + input.value*i);
+        }
     }
 })
